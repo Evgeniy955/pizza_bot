3936548cc6022587ddd0ceeb031bdc24f3f0ff22
Yevhen <galitsyn.evgeniy955@gmail.com>
2024-03-31 18:23:34 +0300

    7 lessons

--- app.py
+++ app.py
@@ -3,29 +3,54 @@ import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, load_dotenv
 
-from common.bot_cmds_list import private
-from handlers.user_group import user_group_router
-from handlers.user_private import user_private_router
+from middlewares.db import DataBaseSession
 
 load_dotenv(find_dotenv())
 
-ALLOWED_UPDATES = ['message, edited_message']
+from database.engine import create_db, drop_db, session_maker
+
+from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from handlers.admin_private import admin_router
+
+from common.bot_cmds_list import private
+
+# ALLOWED_UPDATES = ['message, edited_message', 'callback_query']
 
 bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot.my_admins_list = []
 dp = Dispatcher()
+
+# dp.update.outer_middleware(CounterMiddleware())
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
+dp.include_router(admin_router)
+
+run_param = False
+async def on_startup(bot):
+    if run_param:
+        await drop_db()
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Бот лег')
 
 
 async def main():
+    await create_db()
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+
     '''Чтобы бот не отвечал на сообщения если он офлайн'''
     await bot.delete_webhook(drop_pending_updates=True)
     # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
-    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
 asyncio.run(main())
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,50 +1,63 @@
 from aiogram.filters import CommandStart, Command, or_f
 from aiogram import types, Router, F
 from aiogram.enums import ParseMode
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.orm_query import orm_get_products
 from filters.chat_types import ChatTypeFilter
-from kbds import reply
 from aiogram.utils.formatting import (
     as_list,
     as_marked_section,
     Bold,
 )  # Italic, as_numbered_list и тд
+from kbds.reply import get_keyboard
 
 user_private_router = Router()
-user_private_router.message.filter(ChatTypeFilter(['private']))
+user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    # await message.answer('Привет, я виртуальный помощник', reply_markup=reply.test_kbd)
-    await message.answer('Привет, я виртуальный помощник',
-                         reply_markup=reply.start_kb3.as_markup(
-                             resize_keyboard=True,
-                             input_field_placeholder='Что вас интересует?'
-                         ))
+    # await message.answer("Привет, я виртуальный помощник", reply_markup=reply.test_kbd)
+    await message.answer("Привет, я виртуальный помощник",
+                         # reply_markup=reply.start_kb3.as_markup(
+                         reply_markup=get_keyboard(
+                             "Меню",
+                             "О магазине",
+                             "Варианты оплаты",
+                             "Варианты доставки",
+                             placeholder="Что вас интересует?",
+                             sizes=(2, 2)
+                         ),
+                         )
 
 
-@user_private_router.message(or_f(Command('menu'), F.text.lower() == "меню"))
-async def menu_cmd(message: types.Message):
-    # await bot.send_message(message.from_user.id, 'Ответ')
-    await message.answer("Вот меню:", reply_markup=reply.del_kbd)
+@user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
+async def menu_cmd(message: types.Message, session: AsyncSession):
+    # await bot.send_message(message.from_user.id, "Ответ")
+    for product in await orm_get_products(session):
+        await message.answer_photo(
+            product.image,
+            caption=f"<strong>{product.name}\
+                    </strong>\n{product.description}\nСтоимость: {round(product.price, 2)}",
+        )
+    await message.answer("Вот меню:")
     # await message.reply(message.text)
     # text: str = message.text
-    # if text in ['Привет', 'привет', 'hi', 'Hi', 'Hello']:
-    #     await message.answer('И тебе привет!')
-    # elif text in ['Пока', 'пока', 'пакеда', 'До свидания', 'Goodbye']:
-    #     await message.answer('И тебе пока!')
+    # if text in ["Привет", "привет", "hi", "Hi", "Hello"]:
+    #     await message.answer("И тебе привет!")
+    # elif text in ["Пока", "пока", "пакеда", "До свидания", "Goodbye"]:
+    #     await message.answer("И тебе пока!")
     # else:
     #     await message.answer(message.text)
 
-
-@user_private_router.message(Command('about'))
 @user_private_router.message(F.text.lower() == "о магазине")
+@user_private_router.message(Command("about"))
 async def about_cmd(message: types.Message):
     await message.answer("О нас:")
 
-
-@user_private_router.message(Command('payment'))
 @user_private_router.message(F.text.lower() == "варианты оплаты")
+@user_private_router.message(Command("payment"))
 async def payment_cmd(message: types.Message):
 
     text = as_marked_section(
@@ -52,13 +65,14 @@ async def payment_cmd(message: types.Message):
     "Картой в боте",
         "При получении карта/кеш",
         "В заведении",
-        marker='✅ '
+        marker="✅ ",
     )
     await message.answer(text.as_html())
 
 
-@user_private_router.message(F.text.lower().contains('доставк') | F.text.lower().contains('варианты доставки'))
-@user_private_router.message(Command('shipping'))
+@user_private_router.message(
+    F.text.lower().contains("доставк") | (F.text.lower() == "варианты доставки"))
+@user_private_router.message(Command("shipping"))
 async def filter_cmd(message: types.Message):
 
     text = as_list(as_marked_section(
@@ -66,24 +80,24 @@ async def filter_cmd(message: types.Message):
         "Курьер",
         "Самовынос (сейчас прибегу заберу)",
         "Покушаю у вас (сейчас прибегу)",
-        marker='✅ '
+        marker="✅ "
     ),
         as_marked_section(
             Bold("Нельзя:"),
             "Почта",
             "Голуби",
-            marker='❌ '
+            marker="❌ "
         ),
-        sep='\n---------------------\n'
+        sep="\n---------------------\n"
     )
     await message.answer(text.as_html())
 
-@user_private_router.message(F.contact)
-async def get_contact(message: types.Message):
-    await message.answer("Номер получен")
-    await message.answer(str(message.contact.phone_number))
-
-@user_private_router.message(F.location)
-async def get_location(message: types.Message):
-    await message.answer("Локация получена")
-    await message.answer(str(message.location))
+# @user_private_router.message(F.contact)
+# async def get_contact(message: types.Message):
+#     await message.answer("Номер получен")
+#     await message.answer(str(message.contact.phone_number))
+#
+# @user_private_router.message(F.location)
+# async def get_location(message: types.Message):
+#     await message.answer("Локация получена")
+#     await message.answer(str(message.location))
